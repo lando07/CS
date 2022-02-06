fc9b150252c42db815ee23f80676e1ea150fe00d
lando07 <96402540+lando07@users.noreply.github.com>
2022-02-06 16:51:48 -0600

    fixed some variables

--- MultiThreading/MultithreadPNC.java
+++ MultiThreading/MultithreadPNC.java
@@ -10,27 +10,33 @@ public class MultithreadPNC {
     public static void main(String[] args) {
         String yesOrNo;
         long integer;
-        try(Scanner console = new Scanner(in)){
+        try (Scanner console = new Scanner(in)) {
             long primeCounter = 0;
-            do{
+            do {
                 out.print("Would you like to check the prime numbers between 0 and your number?(yes or no): ");
                 yesOrNo = console.nextLine();
-                yesOrNo.toLowerCase();
-                if(yesOrNo.contains("no"))
+                yesOrNo = yesOrNo.toLowerCase();
+                if (yesOrNo.contains("no"))
                     continue;
                 out.print("Enter an integer to calculate up to: ");
                 integer = console.nextLong();
                 console.nextLine();
-                if(integer % 8 == 0){
-                    out.println("No decmial number found");
-                }
-                else{
+                if (integer % 8 == 0) {
+                    long intDiff = integer / 8;
+                    long[] numArr = new long[8];
+                    for (int i = 0; i < 8; i += intDiff) {
+                        numArr[i] = i + intDiff;
+                    }
+                    for (int i = 0; i < 8; i++) {
+                        out.println(numArr[i]);
+                    }
+                } else {
                     double amountPerThread = integer / (double) 8;
                     double amountToCompensate = amountPerThread - (int) amountPerThread;
                     amountToCompensate *= 8;
                     out.println(amountToCompensate);
                 }
-            }while(yesOrNo.contains("yes"));
+            } while (yesOrNo.contains("yes"));
         }
     }
 }
