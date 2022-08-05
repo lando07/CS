9820375057ecc60a00748fe41775cc221bec13e8
lando07 <96402540+lando07@users.noreply.github.com>
2022-08-04 21:28:17 -0500

    Did some SuperLab

--- SuperLab/LabObj.java
+++ SuperLab/LabObj.java
@@ -1,8 +1,39 @@
 
 public class LabObj {
-
     void run() {
-
+        switch(Objs.getOpt()){
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+        }
     }
 
     private class labs {
@@ -118,6 +149,45 @@ public class LabObj {
 
         }
         private class subLabs{
+            void lab15A(){
+
+            }
+            void lab15B(){
+
+            }
+            void lab23A(){
+
+            } 
+            void lab23B(){
+
+            }
+            void lab30A(){
+
+            }
+            void lab30B(){
+                
+            }
+            void lab30C(){
+                
+            }
+            void lab30D(){
+                
+            }
+            void lab30E(){
+                
+            }
+            void lab30F(){
+                
+            }
+            void lab30G(){
+                
+            }
+            void lab30H(){
+                
+            }
+            void lab30I(){
+                
+            }
 
         }
     }
